9e3347ef4362de18864f8666bbd1c83d81835d1a
Clement Ruhm <clement@balacoon.com>
2022-08-04 14:33:35 +0200

    docker: Add dockerfile and docker image build script

---
+++ docker/build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+# Copyright 2022 Balacoon
+
+set -e
+set -x
+
+usage() {
+    cat <<EOF
+Usage:
+bash $1 [--tag some_tag] [--build-pg] [--no-cache]
+
+Arguments:
+tag - tag of docker image, by default "latest"
+build-pg - pronunciation_package is built from sources (private package)
+no-cache - rebuild all docker layers from scratch
+EOF
+}
+
+docker_image_name="learn_to_pronounce"
+tag="latest"
+build_pg_opt=""
+no_cache_opt=""
+ssh_keys_opt=""
+while [ "$1" != "" ]; do
+    case $1 in
+        --tag )
+            shift
+            tag=$1
+        ;;
+        --build-pg )
+            build_pg_opt="--build-arg build_pg=true"
+        ;;
+        --no-cache )
+	    no_cache_opt="--no-cache"
+	;;
+        -h | --help ) usage $0
+            exit
+        ;;
+        * ) usage $0
+            exit 1
+    esac
+    shift
+done
+
+DOCKER_BUILDKIT=1 docker build --ssh default $no_cache_opt \
+	-t $docker_image_name:$tag $build_pg_opt \
+	-f docker/Dockerfile .
+
